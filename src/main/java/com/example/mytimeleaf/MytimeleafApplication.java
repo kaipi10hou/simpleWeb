1c54d0025c30b8b6140636e03a17f93acd23cc56
CherryOnTop <kaipi10hou@gmail.com>
2021-03-21 19:46:08 +0900

    Initial commit

---
+++ src/main/java/com/example/mytimeleaf/MytimeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytimeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytimeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytimeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytimeleaf/MytimeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytimeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytimeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
